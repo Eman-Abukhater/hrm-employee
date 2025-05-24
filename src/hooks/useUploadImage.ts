f99becc3df3a4cbe2eb42f3fb6e21f8e52ed0d70
Eman <emanabukhater2001@gmail.com>
2025-05-24 19:12:50 +0300

    Cloudinary Image Upload Hook

---
+++ src/hooks/useUploadImage.ts
@@ -0,0 +1,41 @@
+'use client';
+
+import { useState } from 'react';
+
+export const useUploadImage = () => {
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const uploadImage = async (file: File): Promise<string | null> => {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'employee_upload'); 
+    formData.append('folder', 'employees')
+    setUploading(true);
+    setError(null);
+
+    try {
+      const res = await fetch('https://api.cloudinary.com/v1_1/dbjueuler/image/upload', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await res.json();
+
+      if (data.secure_url) {
+        return data.secure_url;
+      } else {
+        setError('Upload failed');
+        return null;
+      }
+    } catch (err) {
+      setError('Upload failed');
+      console.error('Upload error:', err);
+      return null;
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  return { uploadImage, uploading, error };
+};
