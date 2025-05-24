4fd04d5b6d37febe7703befd6bc82e56889cba69
Eman <emanabukhater2001@gmail.com>
2025-05-24 17:42:20 +0300

    Create Zod Schema for the form

---
+++ src/lib/zodSchemas.ts
@@ -0,0 +1,16 @@
+// lib/zodSchemas.ts
+import { z } from "zod";
+
+export const employeeSchema = z.object({
+  fullName: z.string().min(2, "Full name is required"),
+  email: z.string().email("Invalid email"),
+  phone: z
+    .string()
+    .min(10, "Phone must be at least 10 digits")
+    .max(15, "Phone must be under 15 digits"),
+  department: z.string().min(1, "Department is required"),
+  role: z.string().min(1, "Role is required"),
+  joinDate: z.string().min(1, "Join Date is required"),
+  status: z.enum(["active", "inactive"]),
+  profilePhoto: z.string().url().optional(), // Will store Cloudinary URL
+});
